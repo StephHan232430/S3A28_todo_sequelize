eab38acb2018caa2747927a09f10f14a3d7cfa30
Steph <blackman914037@gmail.com>
2019-12-12 10:13:26 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -11,11 +11,28 @@ const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
+const session = require('express-session')
+const passport = require('passport')
+
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'secret',
+  resave: 'false',
+  saveUninitialized: 'false'
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('hello world!')
