728260dd2f6d9d21e6a3b121dcebcc135ea6bbd9
Steph <blackman914037@gmail.com>
2019-12-16 02:19:10 +0800

    fix warning message of register and login routes

--- routes/user.js
+++ routes/user.js
@@ -8,20 +8,22 @@ const User = db.User
 const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
-  res.render('login')
+  const errors = req.flash().error
+  return res.render('login', { errors })
 })
 
 // 登入檢查
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/users/login'
+    failureRedirect: '/users/login',
+    failureFlash: true
   })(req, res, next)
 })
 
 // 註冊頁面
 router.get('/register', (req, res) => {
-  res.render('register')
+  return res.render('register')
 })
 
 // 註冊檢查
@@ -30,45 +32,55 @@ router.post('/register', (req, res) => {
   let errors = []
 
   if (!name || !email || !password || !password2) {
-    errors.push({ message: '所有欄位都是必填' })
+    errors.push('所有欄位都是必填')
   }
 
   if (password !== password2) {
-    errors.push({ message: '密碼輸入錯誤' })
+    errors.push('密碼輸入錯誤')
   }
 
-  User.findOne({ where: { email: email } }).then(user => {
-    if (user) {
-      errors.push({ message: '此email已被註冊使用' })
-      res.render('register', {
-        errors,
-        name,
-        email,
-        password,
-        password2
-      })
-    } else {
-      const newUser = new User({
-        name,
-        email,
-        password
-      })
-
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err
-          newUser.password = hash
-
-          newUser
-            .save()
-            .then(user => {
-              res.redirect('/')
-            })
-            .catch(err => console.log(err))
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    })
+  } else {
+    User.findOne({ where: { email: email } }).then(user => {
+      if (user) {
+        errors.push('此email已被註冊使用')
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
         })
-      })
-    }
-  })
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
+
+            newUser
+              .save()
+              .then(user => {
+                res.redirect('/')
+              })
+              .catch(err => console.log(err))
+          })
+        })
+      }
+    })
+  }
 })
 
 // 登出
