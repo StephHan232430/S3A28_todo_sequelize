abc1389a5d7823c04e7c14f850b9b75d7cdeff39
Steph <blackman914037@gmail.com>
2019-12-15 20:52:16 +0800

    fix warning message of registered email

--- routes/user.js
+++ routes/user.js
@@ -37,41 +37,38 @@ router.post('/register', (req, res) => {
     errors.push({ message: '密碼輸入錯誤' })
   }
 
-  if (errors.length > 0) {
-    res.render('register', { errors, name, email, password, password2 })
-  } else {
-    User.findOne({ where: { email: email } }).then(user => {
-      if (user) {
-        errors.push({ message: '此email已被註冊使用' })
-        res.render('register', {
-          name,
-          email,
-          password,
-          password2
-        })
-      } else {
-        const newUser = new User({
-          name,
-          email,
-          password
-        })
+  User.findOne({ where: { email: email } }).then(user => {
+    if (user) {
+      errors.push({ message: '此email已被註冊使用' })
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err
-            newUser.password = hash
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
 
-            newUser
-              .save()
-              .then(user => {
-                res.redirect('/')
-              })
-              .catch(err => console.log(err))
-          })
+          newUser
+            .save()
+            .then(user => {
+              res.redirect('/')
+            })
+            .catch(err => console.log(err))
         })
-      }
-    })
-  }
+      })
+    }
+  })
 })
 
 // 登出
